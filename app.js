d592e0e9b62c1da06f49b112e9de498dcb48550a
Sarah Tirey <sarah@email.com>
2020-11-07 15:07:44 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const app = express();
+const sequelize = require('./db');
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("The app is listening on port 3000");
+});
